e560354825c63541fdfedd092e292f1f0e66b140
shawkoike <shaw@koikeshou-no-MacBook-Air.local>
2017-03-15 16:45:25 +0900

    done

--- recommend.rb
+++ recommend.rb
@@ -12,22 +12,39 @@ def open_each_ss(file)
   end
 end
 
+# タイトルの把握
+titles = Array.new()
+File.open('./data/titles.txt') do |f|
+  while title = f.gets do
+    titles << title.chomp.to_s
+  end
+end
+
+# url の把握
+urls = Array.new()
+File.open('url.txt',"r") do |file|
+  while url = file.gets do
+    urls << url.chomp.to_s
+  end
+end
+
 data = File.read("./rec/vec.dat")
 vectors = Marshal.load data
 
-# 656番目のSSベクトルデータを格納
-ss_655_vec = Vector.elements(vectors[1])
+# vectors の添え字を変えることでリコメンド元が変更可能 0 ~ 661
+vec = Vector.elements(vectors[365])
 
-max_score = Array.new(3, {score: 0, index: 0})
+max_score = Array.new(6, {score: 0, index: 0, title: "", url: ""})
 
 vectors.each_with_index do |vector, index|
   # ベクトルの内積の和を計算し保存
-  score = ss_655_vec.inner_product(Vector.elements(vector)).fdiv(ss_655_vec.norm * Vector.elements(vector).norm)
+  score = vec.inner_product(Vector.elements(vector)).fdiv(vec.norm * Vector.elements(vector).norm)
   max_score.sort! {|a, b| a[:score] <=> b[:score] }
   # スコアが高い場合は更新
   if max_score[0][:score] < score
     max_score.shift
-    max_score.push({score: score, index: index})
+    # max_score.push({score: score, index: index})
+    max_score.push({score: score, index: index,title: titles[index], url: urls[index]})
   end
 end
 
